4448a176b1b14fdce4049658b573361b3df9dbda
tungd <tungduong2288@gmail.com>
2024-05-22 20:25:11 +0700

    finished

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ import socket
 # Kích thước bảng caro
 BOARD_SIZE = 15
 both_connected = False
-both_connected_lock = threading.Lock()  # Khóa để đồng bộ truy cập vào both_connected
-current_border_id = None        #ID của khung đánh dấu nước đi
+both_connected_lock = threading.Lock()  
+current_border_id = None        
 
 def create_board(size):
     return [[' ' for _ in range(size)] for _ in range(size)]
@@ -60,8 +60,8 @@ def check_win(board, row, col, symbol):
 def draw_board():
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
-            canvas.create_rectangle(j*40, i*40, (j+1)*40, (i+1)*40, outline="black")    #Vẽ khung từng ô
-    canvas.bind("<Button-1>", lambda event: on_click(event))        #Gán hàm xử lý sự kiện cho ô
+            canvas.create_rectangle(j*40, i*40, (j+1)*40, (i+1)*40, outline="black")    
+    canvas.bind("<Button-1>", lambda event: on_click(event))        
 
 
 #Xử lý sự kiện khi click chuột
@@ -69,51 +69,51 @@ def on_click(event):
     global my_turn, game_over, symbol, both_connected, current_border_id
     print(f"both_connected: {both_connected}, my_turn: {my_turn}, game_over: {game_over}")
     with both_connected_lock:
-        if not both_connected:  # Kiểm tra nếu cả hai người chơi chưa kết nối
+        if not both_connected:  
             label.config(text="Waiting for opponent to connect...")
             return
-    #Nếu chưa tới lượt của người chơi thì không thực hiện gì cả
+    
     if not my_turn or game_over:
         return
-    #Lấy tọa độ điểm được đánh
+    
     col = event.x // 40
     row = event.y // 40
-    #Kiểm tra xem tọa độ đã được đánh chưa
+    
     if board[row][col] == ' ':
-        #Xóa khung đánh dấu điểm được đánh trước đó
+        
         if current_border_id:           
             canvas.delete(current_border_id)
-        draw_move(row, col, symbol, "blue")             #Gọi hàm draw_move để vẽ ký hiệu X, O lên khung
-        current_border_id = draw_border(row, col)       #Lấy id khung đánh dấu lượt đánh
+        draw_move(row, col, symbol, "blue")             
+        current_border_id = draw_border(row, col)       
         board[row][col] = symbol
-        #Gọi hàm check_win để kiểm tra xem có người chơi nào thắng không
+        
         if check_win(board, row, col, symbol):
             label.config(text=f"Player {symbol} wins!")
-            send_data(f"WIN{symbol}{row:02}{col:02}")       #Gửi dữ liệu người thắng cho server
+            send_data(f"WIN{symbol}{row:02}{col:02}")       
             game_over = True                
-            reset_button.pack()  #Hiện nút cho phép chơi lại
-        #Nếu chưa có người chơi thắng thì lưu trạng thái đã đến lượt người chơi khác và yêu cầu đợi người chơi khác đánh 
+            reset_button.pack()  
+         
         else:
-            send_data(f"MOVE{row:02}{col:02}")      #Gửi tín hiệu Move và vị trí được đánh đến server
+            send_data(f"MOVE{row:02}{col:02}")     
             my_turn = False
             label.config(text="Waiting for opponent...")
 
-#Vẽ ký hiệu X, O lên khung
-def draw_move(row, col, symbol, color):
-    x = col * 40 + 20           #Lấy tọa độ X
-    y = row * 40 + 20           #Lấy tọa độ Y
-    return canvas.create_text(x, y, text=symbol, font=('Helvetica', 20, 'bold'), fill=color),   #Vẽ
 
-#Vẽ khung đánh dấu vị trí vừa được đánh
+def draw_move(row, col, symbol, color):
+    x = col * 40 + 20          
+    y = row * 40 + 20           
+    return canvas.create_text(x, y, text=symbol, font=('Helvetica', 20, 'bold'), fill=color), 
+
+
 def draw_border(row, col):
-    #Tính tọa độ
+    
     x1 = col * 40       
     y1 = row * 40
     x2 = (col + 1) * 40
     y2 = (row + 1) * 40
-    return canvas.create_rectangle(x1, y1, x2, y2, outline="red", width=2)      #Vẽ
+    return canvas.create_rectangle(x1, y1, x2, y2, outline="red", width=2)   
 
-#Gửi message
+
 def send_data(data):
     try:
         client_socket.send(bytes(data, 'utf-8'))
@@ -122,44 +122,44 @@ def send_data(data):
         print("[ERROR] Connection reset error")
         pass
 
-#Lắng nghe sự kiện
+
 def receive_data():
     global my_turn, game_over, symbol, opponent_symbol, both_connected, current_border_id
     while True:
         try:
-            data = client_socket.recv(1024).decode('utf-8')     #Nhận dữ liệu từ server
+            data = client_socket.recv(1024).decode('utf-8')     
             if data.startswith("SYMBOL"):
-                symbol = data[6]        #Gán symbol bằng ký tự thứ 7 của chuỗi (X hoặc O)
+                symbol = data[6]        
                 if symbol == "O":
                     both_connected = True
-                    chat_entry.config(state=tk.NORMAL)  #Mở khóa khung chat
+                    chat_entry.config(state=tk.NORMAL)  
                 opponent_symbol = 'O' if symbol == 'X' else 'X'
                 my_turn = (symbol == 'X') 
                 label.config(text="Your turn" if my_turn else "Waiting for opponent...")
             elif data.startswith("MOVE"):
-                row = int(data[4:6])        #Lấy tọa độ hàng
-                col = int(data[6:8])        #Lấy tọa độ cột
-                board[row][col] = opponent_symbol       #Lưu dự liệu X, O vào mảng
-                draw_move(row, col, opponent_symbol, "red")     #Vẽ ký hiệu lên khung
-                #Xóa đánh dấu vị trí đánh trước đó nếu tồn tại
+                row = int(data[4:6])        
+                col = int(data[6:8])        
+                board[row][col] = opponent_symbol       
+                draw_move(row, col, opponent_symbol, "red")    
+                
                 if current_border_id:
                     canvas.delete(current_border_id)
-                current_border_id = draw_border(row, col)   #Đánh dấu vị trí mới
-                #Nếu có người thắng thì thông báo thông tin. Lưu tín hiệu game over
+                current_border_id = draw_border(row, col)   
+                
                 if check_win(board, row, col, opponent_symbol):
                     label.config(text=f"Player {opponent_symbol} wins!")
                     game_over = True
-                    reset_button.pack()  # Hiển thị nút chơi lại
+                    reset_button.pack()  
                 else:
                     my_turn = True
                     label.config(text="Your turn")
             elif data.startswith("WIN"):   
-                row = int(data[4:6])        #Lấy tọa độ hàng
-                col = int(data[6:8])        #Lấy tọa độ cột
+                row = int(data[4:6])        
+                col = int(data[6:8])        
                 draw_move(row, col, opponent_symbol, "red")
                 label.config(text=f"Player {data[3]} wins!")
                 game_over = True
-                reset_button.pack()  # Show reset button
+                reset_button.pack() 
             elif data.startswith("RESET"):
                 reset_game()
             elif data.startswith("CHAT"):
@@ -167,8 +167,8 @@ def receive_data():
                 chat_listbox.insert(tk.END, f"Opponent: {chat_message}")
             elif data.startswith("START"):
                 with both_connected_lock:
-                    both_connected = True  # Cập nhật trạng thái khi cả hai người chơi đã kết nối
-                chat_entry.config(state=tk.NORMAL)  # Bật chức năng chat
+                    both_connected = True  
+                chat_entry.config(state=tk.NORMAL) 
                 label.config(text="Both players connected. You can start chatting!")
         except ConnectionResetError:
             print("[ERROR] Connection reset error")
@@ -182,7 +182,7 @@ def reset_game():
     game_over = False
     my_turn = (symbol == 'X')
     label.config(text="Your turn" if my_turn else "Waiting for opponent...")
-    reset_button.pack_forget()  # Hide reset button
+    reset_button.pack_forget() 
     current_border_id = None
 
 def on_reset_click():
--- server.py
+++ server.py
@@ -1,40 +1,40 @@
-import socket               #Tạo và quản lý kết nối
-import threading            #Hỗ trợ xử lý đa luồng
+import socket               
+import threading            
 
 clients = {}
-reset_requests = { 'X': False, 'O': False } #Biến để kiểm tra yêu cầu reset của 2 client
+reset_requests = { 'X': False, 'O': False } 
 
-both_connected = False                      #Biến kiểm tra 2 client đã kết nối chưa
+both_connected = False                     
 both_connected_lock = threading.Lock()      
 
 def handle_client(client_socket, symbol):
     global reset_requests
-    #Lắng nghe sự kiện từ client
+    
     while True:
         try:
-            data = client_socket.recv(1024).decode('utf-8')             #Nhận dữ liệu từ client
+            data = client_socket.recv(1024).decode('utf-8')             
             print(f"[RECEIVED from {symbol}] {data}")
-            if data.startswith("MOVE") or data.startswith("WIN"):       #Kiểm tra dữ liệu gửi từ client có bắt đầu bằng "MOVE" hay "WiN" không
+            if data.startswith("MOVE") or data.startswith("WIN"):       
                 other_symbol = 'O' if symbol == 'X' else 'X'            
-                clients[other_symbol].send(bytes(data, 'utf-8'))        #Gửi data cho client còn lại
-            elif data.startswith("RESET"):                              #Kiểm tra xem dữ liệu gửi từ client có bắt đầu bằng "RESET" không
-                reset_requests[symbol] = True                           #Lưu trạng thái client đã gửi yêu cầu reset ván đấu
-                #Nếu tất cả client đã gửi yêu cầu reset ván đấu thì gửi thông báo RESET đến các client
+                clients[other_symbol].send(bytes(data, 'utf-8'))        
+            elif data.startswith("RESET"):                              
+                reset_requests[symbol] = True                           
+                
                 if all(reset_requests.values()):                        
                     reset_requests = { 'X': False, 'O': False }
                     for client in clients.values():
                         client.send(bytes("RESET", 'utf-8'))
-            elif data.startswith("CHAT"):                               #Kiểm tra xem dữ liệu gửi từ client có bắt đầu bằng "CHAT" không
+            elif data.startswith("CHAT"):            
                 other_symbol = 'O' if symbol == 'X' else 'X'
-                clients[other_symbol].send(bytes(data, 'utf-8'))        #Gửi thông báo "CHAT" đến client còn lại
-        except ConnectionResetError:                                    #Xử lý ngoại lệ
+                clients[other_symbol].send(bytes(data, 'utf-8'))       
+        except ConnectionResetError:                                    
             print(f"[DISCONNECTED] {symbol} disconnected")
             break
 
 def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          #Tạo đối tượng socket sử dụng IPv4 và giao thức TCP
-    server.bind(("0.0.0.0", 12345))                                     #Bắt đầu lắng nghe các kết nối trên địa chỉ IP '0.0.0.0', cổng 12345 (Nghe các kết nối từ tất cả địa chỉ IP)
-    server.listen(2)                                                    #Đưa máy chủ vào chế độ lắng nghe, với tối đa 2 kết nối
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          
+    server.bind(("0.0.0.0", 12345))                                     
+    server.listen(2)                                                    
     print("[STARTED] Server is listening...")
 
     symbol = 'X'                                                        
@@ -42,20 +42,20 @@ def start_server():
         client_socket, addr = server.accept()
         print(f"[CONNECTED] {addr} connected as {symbol}")
         clients[symbol] = client_socket
-        client_socket.send(bytes(f"SYMBOL{symbol}", 'utf-8'))           #Gửi dữ liệu cho client biết về biểu tượng của mình (X hoặc O)
+        client_socket.send(bytes(f"SYMBOL{symbol}", 'utf-8'))          
 
-        # Kiểm tra nếu cả hai người chơi đã kết nối
+   
         if 'X' in clients and 'O' in clients:
             with both_connected_lock:
-                both_connected = True  # Cập nhật trạng thái khi cả hai người chơi đã kết nối
+                both_connected = True  
             for client in clients.values():
-                client.send(bytes("START", 'utf-8'))  # Gửi thông báo "START" đến cả hai client
+                client.send(bytes("START", 'utf-8'))  
             print("Both players connected, sending START signal")
 
         thread = threading.Thread(target=handle_client, args=(client_socket, symbol))
         thread.start()
 
-        symbol = 'O' if symbol == 'X' else 'X'                          #Chuyển symbol thành O nếu đang là X và ngược lại
+        symbol = 'O' if symbol == 'X' else 'X'                          
 
 if __name__ == "__main__":
     start_server()
